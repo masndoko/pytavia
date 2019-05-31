506f43085f9ada82f0f885dfc56a570aa81ddc89
Wahyu Handoko <mas.ndoko@gmail.com>
2019-05-31 14:11:01 +0700

    prepare core & lib

--- pytavia_core/config.py
+++ pytavia_core/config.py
@@ -8,8 +8,6 @@ sys.path.append("pytavia_settings")
 sys.path.append("pytavia_stdlib"  )
 sys.path.append("pytavia_storage" )
 
-from pytavia_stdlib import idgen
-
 G_FLASK_SECRET=b'_5#y2L"F4Q8z\n\xec]/'
 
 # This is the home path for the home of this project
@@ -22,7 +20,7 @@ G_STATIC_URL_PATH           = "/static"
 G_UPLOAD_PATH               = G_HOME_PATH + G_STATIC_URL_PATH + "/upload"
 G_UPLOAD_URL_PATH           = G_STATIC_URL_PATH + "/upload"
 
-mainDB                      = "card-DB"
+mainDB                      = "dbsample"
 mongo_main_db_report_string = "mongodb://127.0.0.1:27017/" + mainDB
 
 # This is where we have all the databases we want to connect to
--- pytavia_core/model.py
+++ pytavia_core/model.py
@@ -8,43 +8,43 @@ from bson.objectid import ObjectId
 
 class mongo_model:
 
-    def __init__(self, record, lookup, db_handle):
-        self._mongo_record  = copy.deepcopy(record)
-        self._lookup_record = copy.deepcopy(lookup)
-        self._db_handle     = db_handle
-    # end def
+	def __init__(self, record, lookup, db_handle):
+		self._mongo_record  = copy.deepcopy(record)
+		self._lookup_record = copy.deepcopy(lookup)
+		self._db_handle     = db_handle
+	# end def
 
-    def put(self, key, value):
-        if not (key in self._lookup_record):
-            raise ValueError('SETTING_NON_EXISTING_FIELD', key, value)
-        # end if
-        self._mongo_record[key] = value
-    # end def
+	def put(self, key, value):
+		if not (key in self._lookup_record):
+			raise ValueError('SETTING_NON_EXISTING_FIELD', key, value)
+		# end if
+		self._mongo_record[key] = value
+	# end def
 
-    def get(self):
-        return self._mongo_record
-    # end def	
+	def get(self):
+		return self._mongo_record
+	# end def	
 
-    def delete(self , query):
-        collection_name = self._lookup_record["__db__name__"]
-        self._db_handle[collection_name].remove( query )
-    # end def
+	def delete(self , query):
+		collection_name = self._lookup_record["__db__name__"]
+		self._db_handle[collection_name].remove( query )
+	# end def
 
-    def insert(self):
-        collection_name = self._lookup_record["__db__name__"]
-        del self._mongo_record["__db__name__"]
-        self._db_handle[collection_name].insert(  
-            self._mongo_record
-        )
-    # end def
+	def insert(self):
+		collection_name = self._lookup_record["__db__name__"]
+		del self._mongo_record["__db__name__"]
+		self._db_handle[collection_name].insert(  
+			self._mongo_record
+		)
+	# end def
 
-    def update(self, query):
-        collection_name = self._lookup_record["__db__name__"]
-        self._db_handle[collection_name].update(
-            query, 
-            { "$set" : self._mongo_record }
-        )
-    # end def
+	def update(self, query):
+		collection_name = self._lookup_record["__db__name__"]
+		self._db_handle[collection_name].update(
+			query, 
+			{ "$set" : self._mongo_record }
+		)
+	# end def
 # end class
 #
 #
@@ -52,12 +52,13 @@ class mongo_model:
 #
 
 db = {
-    "db_card_register"              : {
-        "fk_user_id"                : "",
-        "fk_wallet_id"              : "",
-        "card_payment_gateway"      : "",
-        "response_data"             : "",
-        "initial_amount"            : 0 ,
-        "unique_hash"               : ""
-    }
+
+	#detail table of t_user
+	"t_user" : {
+		"id_user"		: "",
+		"username"		: "",
+		"password"		: "",
+		"salt"			: "",
+		"name"			: ""
+	},
 }
